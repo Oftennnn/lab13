56204dc32df40d032268ff6ca5de6114ce7d29c1
Oftennnn <inwture133@gmail.com>
2024-12-27 22:02:03 +0700

    lab13 easy

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,5 +17,40 @@ int main(){
     cout << "Harmonic Mean = " << B[3] << endl;
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
-    return 0;
+
+   
+}
+
+void stat(const double A[],int N,double B[]){
+    double sum,sum2,invmul,mean,stdev,geomean,harmonic = 0 ;
+    double mul = 1 ;
+    for(int i=0 ; i<N ; i++ ){
+    
+    sum += A[i] ;
+    sum2 += pow(A[i],2) ;
+    mul *= A[i] ;
+    invmul += 1.0/A[i] ;
+    mean = sum/N ;
+    stdev = pow((sum2/N)-pow(mean,2),0.5);
+    cout << mul << " " << geomean << endl;
+    geomean = pow(mul,1.0/N) ;
+    harmonic = N/invmul ;
+
+    double max,min = A[0] ;
+    if(A[i]>max){ max = A[i] ; }
+    if(A[i<min]){ min = A[i] ; }
+
+
+    
+
+    B[0] = mean  ; 
+    B[1] = stdev ;
+    B[2] = geomean ;
+    B[3] = harmonic ;
+    B[4] = max ;
+    B[5] = min ;
+    }
+   
+    
+  
 }
